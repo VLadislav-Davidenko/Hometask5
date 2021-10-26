19b08165c36c28f0023a36ababd0427869a6080e
Visl <5vladik068@gmail.com>
2021-10-26 18:13:23 +0300

    Circle OOP

---
+++ src/com/company/Circle.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.lang.Math;
+
+public class Circle {
+    double radius;
+    double length;
+    double area;
+    String colour;
+
+
+    public Circle(double radius) {
+        this.radius = radius;
+        length = 2 * Math.PI * radius;
+        area = Math.PI * Math.pow(radius, 2);
+    }
+
+    public Circle(double radius, String colour) {
+        this.radius = radius;
+        this.colour = colour;
+        length = 2 * Math.PI * radius;
+        area = Math.PI * Math.pow(radius, 2);
+    }
+
+    public static void answer(Circle data){
+        if (data.colour == null) {
+            System.out.println("R = " + data.radius + "\n C = " + data.length + "\n S = " + data.area + "\n\n");
+        } else{
+            System.out.println("R = " + data.radius + "\n C = " + data.length + "\n S = " + data.area + "\nCol = " + data.colour + "\n\n");
+        }
+
+
+    }
+}
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import com.company.Circle;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Circle krug1 = new Circle(12.3);
+        Circle krug2 = new Circle(4.8, "Dark Blue");
+
+        Circle.answer(krug1);
+        Circle.answer(krug2);
+
+    }
+}
